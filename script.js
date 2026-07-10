934e841db384c239d256ea724db8dbf5a03a6070
Maliyito <malillanyhernol@gmail.com>
2026-07-09 20:40:17 -0600

    Refactor event listeners and update message display

--- script.js
+++ script.js
@@ -1,33 +1,65 @@
 const boton = document.getElementById("boton");
+
 const inicio = document.getElementById("inicio");
+
 const historia = document.getElementById("historia");
 
-boton.addEventListener("click", () => {
-    inicio.style.display = "none";
-   historia.classList.remove("oculto");
+const continuar = document.getElementById("continuar");
+
+const mundo1 = document.getElementById("mundo1");
+
+const texto = document.getElementById("texto");
+
+
+
+boton.addEventListener("click",()=>{
+
+    inicio.style.display="none";
+
+    historia.style.display="flex";
+
+    escribir();
+
 });
+
+
 
 function escribir(){
 
-    let mensaje = 
-    "¡Guau! 🐾\n\nHola Chime...\n\nEstaba esperando por ti.\n\nEste pequeño mundo fue creado especialmente para ti.\n\n¿Lista para comenzar tu aventura?";
+let mensaje = 
+"¡Guau! 🐾\n\nHola Chime...\n\nEstaba esperando por ti.\n\nEste pequeño mundo fue creado especialmente para ti.\n\n¿Lista para comenzar tu aventura? ✨";
 
-    let i = 0;
 
-    texto.innerHTML = "";
+let i=0;
 
-    let intervalo = setInterval(() => {
+texto.innerHTML="";
 
-        texto.innerHTML += mensaje[i];
 
-        i++;
+let intervalo=setInterval(()=>{
 
-        if(i >= mensaje.length){
+texto.innerHTML += mensaje[i];
 
-            clearInterval(intervalo);
+i++;
 
-        }
 
-    },50);
+if(i>=mensaje.length){
+
+clearInterval(intervalo);
 
 }
+
+
+},50);
+
+
+}
+
+
+
+continuar.addEventListener("click",()=>{
+
+historia.style.display="none";
+
+mundo1.style.display="flex";
+
+});
