20cb6b297574aac9814d3ae2c2906a38b18ca250
Maliyito <malillanyhernol@gmail.com>
2026-07-09 18:35:02 -0600

    Add interactive story feature in script.js

---
+++ script.js
@@ -0,0 +1,74 @@
+const boton = document.getElementById("boton");
+
+const inicio = document.getElementById("inicio");
+
+const historia = document.getElementById("historia");
+
+
+const texto = document.getElementById("texto");
+
+const continuar = document.getElementById("continuar");
+
+
+
+boton.addEventListener("click",()=>{
+
+
+    inicio.style.display="none";
+
+    historia.classList.remove("oculto");
+
+
+    escribir();
+
+
+});
+
+
+
+
+
+function escribir(){
+
+
+let mensaje = 
+"¡Guau! 🐾\n\nHola Chime...\n\nEstaba esperando por ti.\n\nEste pequeño mundo fue creado especialmente para ti.\n\n¿Lista para comenzar tu aventura?";
+
+
+let i=0;
+
+
+texto.innerHTML="";
+
+
+let intervalo=setInterval(()=>{
+
+
+    texto.innerHTML += mensaje[i];
+
+
+    i++;
+
+
+    if(i>=mensaje.length){
+
+        clearInterval(intervalo);
+
+    }
+
+
+},50);
+
+
+
+}
+
+
+
+continuar.addEventListener("click",()=>{
+
+
+    alert("Próximamente: El mundo de Press Start 🌙🐶");
+
+
+});
