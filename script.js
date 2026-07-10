1db5e86d594b18664ab7687aba020e45c2be75fe
Maliyito <malillanyhernol@gmail.com>
2026-07-09 20:08:59 -0600

    Implement animated welcome message function
    Add function to display a welcome message with animation.

--- script.js
+++ script.js
@@ -6,3 +6,28 @@ boton.addEventListener("click", () => {
     inicio.style.display = "none";
     historia.style.display = "block";
 });
+
+function escribir(){
+
+    let mensaje = 
+    "¡Guau! 🐾\n\nHola Chime...\n\nEstaba esperando por ti.\n\nEste pequeño mundo fue creado especialmente para ti.\n\n¿Lista para comenzar tu aventura?";
+
+    let i = 0;
+
+    texto.innerHTML = "";
+
+    let intervalo = setInterval(() => {
+
+        texto.innerHTML += mensaje[i];
+
+        i++;
+
+        if(i >= mensaje.length){
+
+            clearInterval(intervalo);
+
+        }
+
+    },50);
+
+}
