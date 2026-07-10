943279b4de3e8fc54013bd441485f0d05581f910
Maliyito <malillanyhernol@gmail.com>
2026-07-09 20:33:15 -0600

    Remove 'oculto' class to show historia element

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const historia = document.getElementById("historia");
 
 boton.addEventListener("click", () => {
     inicio.style.display = "none";
-    historia.style.display = "block";
+   historia.classList.remove("oculto");
 });
 
 function escribir(){
