62edb7d53d761527e8b00cce98f7ba97c4593227
Maliyito <malillanyhernol@gmail.com>
2026-07-09 19:44:57 -0600

    Refactor button click event to show history

--- script.js
+++ script.js
@@ -1,69 +1,8 @@
 const boton = document.getElementById("boton");
-
 const inicio = document.getElementById("inicio");
-
 const historia = document.getElementById("historia");
 
-
-const texto = document.getElementById("texto");
-
-const continuar = document.getElementById("continuar");
-
-
-
-boton.addEventListener("click",()=>{
-
-
-    inicio.style.display="none";
-
-    historia.classList.remove("oculto");
-
-
-    escribir();
-
-
-});
-
-
-
-
-
-function escribir(){
-
-
-let mensaje = 
-"¡Guau! 🐾\n\nHola Chime...\n\nEstaba esperando por ti.\n\nEste pequeño mundo fue creado especialmente para ti.\n\n¿Lista para comenzar tu aventura?";
-
-
-let i=0;
-
-
-texto.innerHTML="";
-
-
-let intervalo=setInterval(()=>{
-
-
-    texto.innerHTML += mensaje[i];
-
-
-    i++;
-
-
-    if(i>=mensaje.length){
-
-        clearInterval(intervalo);
-
-    }
-
-
-},50);
-
-
-continuar.addEventListener("click",()=>{
-
-    historia.style.display="none";
-
-    document.getElementById("mundo1").style.display="block";
-
+boton.addEventListener("click", () => {
+    inicio.style.display = "none";
+    historia.style.display = "block";
 });
